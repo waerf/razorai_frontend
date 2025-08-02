3970b0342db7ea3d4be9f8c5efe387a8069965a2
Betagoe <2353604@tongji.edu.cn>
2025-08-02 20:47:56 +0800

    配置文件

---
+++ RAZOR-AI--Frontend-main/.eslintrc.js
@@ -0,0 +1,49 @@
+// src/.eslintrc.js
+// 这是一个ESLint配置文件，用于定义代码规范。ESLint是一个用于识别和报告代码中的模式的工具，它可以帮助我们保持代码风格的一致性。
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+        tabWidth: 2,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off',
+      },
+    },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
